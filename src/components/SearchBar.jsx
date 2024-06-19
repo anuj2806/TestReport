cdb93f190a699451250fe3151f456b83f40d87e0
anuj00069 <pratap00069@gmail.com>
2024-06-19 13:40:33 +0530

    ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,8 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import MicNoneIcon from '@mui/icons-material/MicNone';
-export default function SearchBar() {
+export default function SearchBar({searchValue}) {
+const [search,setSearch]=React.useState();
   return (
     <Paper
       component="form"
@@ -14,13 +15,15 @@ export default function SearchBar() {
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search Customer"
-        inputProps={{ 'aria-label': 'search google maps' }}
+       value={search}
+       name='customer'
+       onChange={(e)=>setSearch(e.target.value)}
       />
       <IconButton type="button" sx={{ p: '10px'}} aria-label="search">
         <MicNoneIcon />
       </IconButton>
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton  color="primary" sx={{ p: '10px'}} aria-label="directions">
+      <IconButton  color="primary" sx={{ p: '10px'}} aria-label="directions" onClick={()=>searchValue(search)}>
         <SearchIcon color='primary'/>
       </IconButton>
     </Paper>
